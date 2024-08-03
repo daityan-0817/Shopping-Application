75e7c96fac3c7e2ec128d174ba7475349085c012
daityan-0817 <167166634+daityan-0817@users.noreply.github.com>
2024-08-03 13:49:33 +0900

    0803 change

--- Completed/index.js
+++ Completed/index.js
@@ -2,10 +2,10 @@ const Return_to_cart_button = qs('#Return_to_cart')
 const Push_decoration_Return_to_shopping = qs('#return')
 
 Return_to_cart_button.addEventListener('click', () => {
-    location.href = 'http://127.0.0.1:5500/Shopping_application/Productpage/index.html'
+    location.href = 'http://127.0.0.1:5500/Shopping-application/Productpage/index.html'
 })
 
 Push_decoration_Return_to_shopping.addEventListener('click', () => {
-    location.href = 'http://127.0.0.1:5500/Shopping_application/Productpage/index.html'
+    location.href = 'http://127.0.0.1:5500/Shopping-application/Productpage/index.html'
 })
 
