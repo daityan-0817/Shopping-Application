685257b175613515c4cc0ba4fb337dd4a80f1cd1
daityan-0817 <167166634+daityan-0817@users.noreply.github.com>
2024-09-09 17:21:47 +0900

    2024/09/09/change_js

--- index.js
+++ index.js
@@ -5,14 +5,21 @@ const layout = qs('#layout')
 const cartButton = qs('#image_cart')
 const cartNum = 'cartnum'
 const itemStock = 'item_stock'
+const defaultDate = getItemData()
+const newDate = itemStockDate()
+const itemNameList = []
 
 function itemDisplay() {
 
-  const newDate = itemStockDate()
+  for(let item of defaultDate) {
+    itemNameList.push(item.itemName)
+  }  
 
   if(!newDate) {
     
-    for(let item of __item_data) {
+    for(let item of defaultDate) {
+
+      if(item.itemName.includes(searchInput.value)) {
 
         const listItem = document.createElement('div')
         listItem.setAttribute('class','items')
@@ -27,17 +34,13 @@ function itemDisplay() {
         listItem.addEventListener('click', () => {
           location.href =  `/detail/?id=${item.id}&imgsrc=${item.image}=${item.itemName}=${item.price}`
         })
-
-        function itemDisplayClose() {
-          
-          items.removeChild(listItem)
-          searchButton.removeEventListener('click', itemDisplayClose)
       }
-          searchButton.addEventListener('click', itemDisplayClose)
-    } 
-  } else {
+    }
+  }else {
 
     for(let item of newDate) {
+
+      if(item.itemName.includes(searchInput.value)) {
 
         const soldOutItem = [item.stock].includes(0)
 
@@ -57,23 +60,33 @@ function itemDisplay() {
           soldOutMessage.setAttribute('class','soldout_message')
           soldOutMessage.innerText = 'sold out'
           listItem.appendChild(soldOutMessage)
-      } else {
+        }else {
 
-        listItem.addEventListener('click', () => {
-          location.href =  `/detail/?id=${item.id}&imgsrc=${item.image}=${item.itemName}=${item.price}`
-        })
-      }
-        function itemDisplayClose() {
-          
-          items.removeChild(listItem)
-          searchButton.removeEventListener('click', itemDisplayClose)
-      }
-          searchButton.addEventListener('click', itemDisplayClose)
+          listItem.addEventListener('click', () => {
+            location.href =  `/detail/?id=${item.id}&imgsrc=${item.image}=${item.itemName}=${item.price}`
+          })
+        }
+      }    
     }
   }
 }
 
 itemDisplay()
+
+searchButton.addEventListener('click', () => {
+
+  items.innerHTML = ''
+
+  itemDisplay()
+
+  if(!itemNameList.some(elem => elem.includes(searchInput.value))) {
+    const noItemDisplay = document.createElement('span')
+    noItemDisplay.setAttribute('class', 'noitem_display')
+    noItemDisplay.innerText = '検索条件に該当する商品がありません'
+    items.appendChild(noItemDisplay) 
+  }
+
+})
 
 function getCartNum() {
   
@@ -96,173 +109,6 @@ function itemStockDate() {
   return JSON.parse(date)
 }
  
-searchButton.addEventListener('click', () => {
-
-  const newDate = itemStockDate()
-  const itemNameList = []
-
-  if(!newDate) {
-
-    for(let item of __item_data) {
-    
-      itemNameList.push(item.itemName)
-
-      if(searchInput.value === '') {
-        
-        const listItem = document.createElement('div')
-        listItem.setAttribute('class','items')
-        listItem.innerHTML = 
-        ` 
-        <img src="${item.image}"/>
-        <di>${item.itemName}</div>
-        <div>￥${item.price}</div>
-        `
-        items.appendChild(listItem)
-
-        function itemDisplayClose() {
-          
-          items.removeChild(listItem)
-          searchButton.removeEventListener('click', itemDisplayClose)
-        }
-          searchButton.addEventListener('click', itemDisplayClose)
-
-        listItem.addEventListener('click', () => {
-          location.href =  `/detail/?id=${item.id}&imgsrc=${item.image}=${item.itemName}=${item.price}`
-        })
-      } else if(item.itemName.includes(searchInput.value)){
-        
-        const listItem = document.createElement('div')
-        listItem.setAttribute('class','items')
-        listItem.innerHTML = 
-        ` 
-        <img src="${item.image}"/>
-        <di>${item.itemName}</div>
-        <div>￥${item.price}</div>
-        `
-        items.appendChild(listItem)
-
-        function itemDisplayClose() {
-          
-          items.removeChild(listItem)
-          searchButton.removeEventListener('click', itemDisplayClose)
-          }
-          searchButton.addEventListener('click', itemDisplayClose)
-
-        listItem.addEventListener('click', () => {
-          location.href =  `/detail/?id=${item.id}&imgsrc=${item.image}=${item.itemName}=${item.price}`
-        })
-      }
-    } 
-    
-    const noItem = itemNameList.some(elem => elem.includes(searchInput.value))
-    
-    if(searchInput.value !== '' && !noItem) {
-
-      const noItemDisplay = document.createElement('span')
-      noItemDisplay.setAttribute('class', 'noitem_display')
-      noItemDisplay.innerText = '検索条件に該当する商品がありません'
-      items.appendChild(noItemDisplay) 
-
-      function noItemDisplayClose() {
-          
-        items.removeChild(noItemDisplay)
-        searchButton.removeEventListener('click', noItemDisplayClose)
-    }
-        searchButton.addEventListener('click', noItemDisplayClose)
-  }
-}
-
-  if(newDate) {
-
-    for(let item of newDate) {
-
-      const soldOutItem = [item.stock].includes(0)
-      itemNameList.push(item.itemName)
-
-      if(searchInput.value === '') {
-    
-        const listItem = document.createElement('div')
-        listItem.setAttribute('class','items')
-        listItem.innerHTML = 
-        ` 
-        <img src="${item.image}"/>
-        <di>${item.itemName}</div>
-        <div>￥${item.price}</div>
-        `
-        items.appendChild(listItem)
-
-        function itemDisplayClose() {
-          
-          items.removeChild(listItem)
-          searchButton.removeEventListener('click', itemDisplayClose)
-        }
-          searchButton.addEventListener('click', itemDisplayClose)
-
-        if(soldOutItem) {
-      
-          const soldOutMessage = document.createElement('div')
-          soldOutMessage.setAttribute('class','soldout_message')
-          soldOutMessage.innerText = 'sold out'
-          listItem.appendChild(soldOutMessage)
-      } else {
-      
-          listItem.addEventListener('click', () => {
-            location.href =  `/detail/?id=${item.id}&imgsrc=${item.image}=${item.itemName}=${item.price}`
-      })
-    }
-  } else if(item.itemName.includes(searchInput.value)){
-    
-      const listItem = document.createElement('div')
-      listItem.setAttribute('class','items')
-      listItem.innerHTML = 
-      ` 
-      <img src="${item.image}"/>
-      <di>${item.itemName}</div>
-      <div>￥${item.price}</div>
-      `
-      items.appendChild(listItem)
-
-      function itemDisplayClose() {
-          
-        items.removeChild(listItem)
-        searchButton.removeEventListener('click', itemDisplayClose)
-    }
-        searchButton.addEventListener('click', itemDisplayClose)
-
-      if(soldOutItem) {
-      
-        const soldOutMessage = document.createElement('span')
-        soldOutMessage.setAttribute('class','soldout_message')
-        soldOutMessage.innerText = 'sold out'
-        listItem.appendChild(soldOutMessage)
-    } else {
-      
-        listItem.addEventListener('click', () => {
-          location.href =  `/detail/?id=${item.id}&imgsrc=${item.image}=${item.itemName}=${item.price}`
-      })
-    }
-  }
-} 
-
-  const noItem = itemNameList.some(elem => elem.includes(searchInput.value))
-
-  if(searchInput.value !== '' && !noItem) {
-
-      const noItemDisplay = document.createElement('span')
-      noItemDisplay.setAttribute('class', 'noitem_display')
-      noItemDisplay.innerText = '検索条件に該当する商品がありません'
-      items.appendChild(noItemDisplay) 
-
-      function noItemDisplayClose() {
-          
-        items.removeChild(noItemDisplay)
-        searchButton.removeEventListener('click', noItemDisplayClose)
-    }
-        searchButton.addEventListener('click', noItemDisplayClose)
-    }
-  }
-})
-
 cartButton.addEventListener('click', () => {
 
   const date = localStorage.getItem(cartNum)
